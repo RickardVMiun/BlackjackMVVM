6733830d174a240ac1703345dc2ab4c2aa4b6c15
Elin Olovsson <elin_olovsson@hotmail.com>
2021-09-09 10:43:18 +0200

    Refaktorerat

---
+++ Blackjack MVVM/Data/Cards.cs
@@ -0,0 +1,42 @@
+﻿using Blackjack_MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Blackjack_MVVM.Data
+{
+    public class Cards : BaseViewModel
+    {
+        private static readonly Random random = new Random();
+
+        public int CardValue { get; set; }
+        public string CardSuit { get; set; }
+
+        public ObservableCollection<Cards> deckofcards { get; set; }
+
+        private void FillDeck() // Fyller lista med kort
+        {
+            deckofcards = new ObservableCollection<Cards>();
+            {
+
+               
+
+
+            };
+
+        }
+
+        //public int SetCardValue()
+        //{ 
+
+        //}
+        //public int GetCard()
+        //{
+        //    int returnCard = Cards[random.Next(1, 10)];
+
+        //    return returnCard;
+        //}
+
+    }
+}
