29eb20b1d06dabd25973c5f9b84068cc28ee8789
RickardVMiun <svenstaviken@hotmail.com>
2021-09-23 11:43:42 +0200

    Made separate views for hit/stand-button and uploaded for team members to adjust

---
+++ Blackjack MVVM/Views/HitButton.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Blackjack_MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for HitButton.xaml
+    /// </summary>
+    public partial class HitButton : UserControl
+    {
+        public HitButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
