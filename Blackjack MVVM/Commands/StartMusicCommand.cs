eaea279faf0dbefe7e9b505b175c9c0133b6204b
Juno Settfors <junofallbom@hotmail.com>
2021-09-24 10:11:42 +0200

    Created commands for starting and stopping music

---
+++ Blackjack MVVM/Commands/StartMusicCommand.cs
@@ -0,0 +1,32 @@
+﻿using Blackjack_MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Blackjack_MVVM.Commands
+{
+    public class StartMusicCommand : ICommand
+    {
+        private GameViewModel gameViewModel;
+        public event EventHandler CanExecuteChanged;
+        MainWindow mainWindow;
+
+        public StartMusicCommand(GameViewModel gameViewModel)
+        {
+            this.gameViewModel = gameViewModel;
+           
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            mainWindow = new MainWindow();
+            mainWindow.PlayMusic();
+        }
+    }
+}
---
+++ Blackjack MVVM/Commands/StopMusicCommand.cs
@@ -0,0 +1,31 @@
+﻿using Blackjack_MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Blackjack_MVVM.Commands
+{
+    public class StopMusicCommand : ICommand
+    {
+        private GameViewModel gameViewModel;
+        public event EventHandler CanExecuteChanged;
+        MainWindow mainWindow;
+
+        public StopMusicCommand(GameViewModel gameViewModel)
+        {
+            this.gameViewModel = gameViewModel; 
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            mainWindow = new MainWindow();
+            mainWindow.PauseMusic();
+        }
+    }
+}
